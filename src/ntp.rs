1dd8d925e2b18d275a265ef4b2d301e2b4c736db
Will Hawkins <hawkinsw@obs.cr>
2025-04-30 18:38:58 -0400

    Clean NTP Time Format Handling
    Add a method to convert from an NtpTime back to nanoseconds and add
    much-needed tests.
    
    Signed-off-by: Will Hawkins <hawkinsw@obs.cr>

--- src/ntp.rs
+++ src/ntp.rs
@@ -70,22 +70,31 @@ impl NtpTime {
     }
 
     pub fn from_nanos(src_nanos: u64) -> NtpTime {
-        let seconds_in_nanos = src_nanos / NANOSECONDS_PER_SECOND;
+        let seconds = src_nanos / NANOSECONDS_PER_SECOND;
 
-        let mut nanos = src_nanos - (seconds_in_nanos * NANOSECONDS_PER_SECOND);
+        let mut nanos = src_nanos - (seconds * NANOSECONDS_PER_SECOND);
         // Goal: Convert the number of nanoseconds to a fraction of seconds. The fraction
         // is in units of 1/(2^32). So, if there are NANOSECONDS_PER_SECOND nanoseconds
-        // per second, then nanos/NANOSECONDS_PER_SECOND is the fractions of a second. To
-        // convert that into units of 1/(2^32), we multiply by 2^32. Because * is commutative,
+        // per second, then nanos/NANOSECONDS_PER_SECOND is the fractions of a second.NTP Time
+        // is based on fractions of a second that are 1/2^32. That means that there are a total
+        // of 2^32 fractional seconds in a whole second. Multiplying by the fractional nanos/second
+        // will get us where we need to go. Because * is commutative,
         // we can do them in any order, so we will do the multiply first and the divide second.
         // Notes taken from: https://tickelton.gitlab.io/articles/ntp-timestamps/
         nanos <<= 32;
         nanos /= NANOSECONDS_PER_SECOND;
 
         NtpTime {
-            seconds: seconds_in_nanos as u32,
+            seconds: seconds as u32,
             fractions: nanos as u32,
         }
+    }
+
+    pub fn to_nanos(ntp_time: NtpTime) -> u64 {
+        let nanos = (ntp_time.fractions as u64 * NANOSECONDS_PER_SECOND) >> 32;
+        let seconds = ntp_time.seconds as u64 * NANOSECONDS_PER_SECOND;
+
+        seconds + nanos
     }
 }
 
@@ -112,43 +121,6 @@ impl TryFrom<&[u8]> for NtpTime {
     }
 }
 
-#[test]
-fn test_simple_ntp_conversion() {
-    // Based upon the test code from the Apache Commons (file: )
-    // > For example, Tue, Dec 17 2002 09:07:24.810 EST is represented by a single Java-based time value
-    // > of f22cd1fc8a, but its NTP equivalent are all values ranging from c1a9ae1c.cf5c28f5 to c1a9ae1c.cf9db22c.
-    // So, that's our test case.
-    let then = Utc.with_ymd_and_hms(1900, 1, 1, 0, 0, 0).unwrap();
-    // Build Dec 17 2002 09:07:24.810 EST
-    let test = Utc.with_ymd_and_hms(2002, 12, 17, 14, 7, 24).unwrap();
-    let extra_nanos = (0.810f64 * NANOSECONDS_PER_SECOND as f64) as i64;
-
-    let difference = test.signed_duration_since(then).num_nanoseconds().unwrap() + extra_nanos;
-    let result = NtpTime::from_nanos(difference as u64);
-
-    let expected_seconds = 0xc1a9ae1cu32;
-    let expected_fractions = 0xcf5c28f5u32;
-    if result.seconds != expected_seconds {
-        panic!(
-            "Expected {:x} seconds but got {:x}",
-            expected_seconds, result.seconds
-        );
-    }
-    if result.fractions != expected_fractions {
-        panic!(
-            "Expected {:x} fractions but got {:x}",
-            expected_fractions, result.fractions
-        );
-    }
-}
-
-#[test]
-fn test_now_ntp_conversion() {
-    // This is more of a tool to help us visualize the NTP timestamp.
-    let ntp_now = NtpTime::now();
-    println!("ntp_now: {:?}", ntp_now);
-}
-
 impl From<&NtpTime> for Vec<u8> {
     fn from(raw: &NtpTime) -> Vec<u8> {
         let mut result = vec![0u8; 8];
@@ -165,29 +137,173 @@ impl From<NtpTime> for Vec<u8> {
     }
 }
 
-#[test]
-fn simple_ntp_roundtrip_test() {
-    let mut raw_data: [u8; 8] = [0; 8];
-    raw_data[0..4].copy_from_slice(&u32::to_be_bytes(5));
-    raw_data[4..8].copy_from_slice(&u32::to_be_bytes(6));
+#[cfg(test)]
+mod ntptime_tests {
 
-    let ntp_time: Result<NtpTime, NtpError> = raw_data.as_slice().try_into();
+    #[test]
+    fn test_simple_ntp_conversion() {
+        // Based upon the test code from the Apache Commons (file: )
+        // > For example, Tue, Dec 17 2002 09:07:24.810 EST is represented by a single Java-based time value
+        // > of f22cd1fc8a, but its NTP equivalent are all values ranging from c1a9ae1c.cf5c28f5 to c1a9ae1c.cf9db22c.
+        // So, that's our test case.
+        let then = Utc.with_ymd_and_hms(1900, 1, 1, 0, 0, 0).unwrap();
+        // Build Dec 17 2002 09:07:24.810 EST (Note: Using hour = 14 because of conversion between EST and GMT)
+        let test = Utc.with_ymd_and_hms(2002, 12, 17, 14, 7, 24).unwrap()
+            + Duration::from_nanos(810000000);
 
-    if ntp_time.is_err() {
-        panic!(
-            "Should have been able to convert: {}",
-            ntp_time.unwrap_err()
-        );
+        let difference = test.signed_duration_since(then).num_nanoseconds().unwrap();
+        let result = NtpTime::from_nanos(difference as u64);
+
+        let expected_seconds = 0xc1a9ae1cu32;
+        let expected_fractions = 0xcf5c28f5u32;
+        if result.seconds != expected_seconds {
+            panic!(
+                "Expected {:x} seconds but got {:x}",
+                expected_seconds, result.seconds
+            );
+        }
+        if result.fractions != expected_fractions {
+            panic!(
+                "Expected {:x} fractions but got {:x}",
+                expected_fractions, result.fractions
+            );
+        }
     }
 
-    let ntp_time = ntp_time.unwrap();
-    assert_eq!(ntp_time.seconds, 5);
-    assert_eq!(ntp_time.fractions, 6);
+    use crate::ntp::NtpTime;
+    #[cfg(test)]
+    use chrono::DateTime;
+    use chrono::{TimeZone, Utc};
+    use std::time::Duration;
 
-    let ntp_time_serialized: Vec<u8> = ntp_time.into();
+    fn do_test_simple_ntp_conversion_roundtrip(expected: NtpTime, low_date: DateTime<Utc>) {
+        let then = Utc.with_ymd_and_hms(1900, 1, 1, 0, 0, 0).unwrap();
+        let low_nanos = low_date
+            .signed_duration_since(then)
+            .num_nanoseconds()
+            .unwrap();
+        let high_nanos = low_nanos + 1;
 
-    assert!(raw_data[0..4].eq(&ntp_time_serialized[0..4]));
-    assert!(raw_data[4..8].eq(&ntp_time_serialized[4..8]));
+        let low = NtpTime::from_nanos(low_nanos as u64);
+        let high = NtpTime::from_nanos(high_nanos as u64);
+
+        // Seconds should match expected NTP seconds perfectly ...
+        if low.seconds != expected.seconds {
+            panic!(
+                "Expected {:x} seconds but got {:x}",
+                expected.seconds, low.seconds
+            );
+        }
+        // and low/high should span the expected NTP fractions.
+        if !(low.fractions < expected.fractions && expected.fractions <= high.fractions) {
+            // Note: These are inclusive in the case where the next 1/(2^32) fraction
+            // of a second would result in the nanosecond increasing by one
+            // (see simple_ntp_conversion_from_bytes).
+            panic!(
+                "Expected {:x} to be between {:x} and {:x}, but it is not",
+                expected.fractions, high.fractions, high.fractions
+            );
+        }
+
+        let then = Utc.with_ymd_and_hms(1900, 1, 1, 0, 0, 0).unwrap();
+        // Now, "roundtrip" the expected result back to a DateTime and make sure that it is _precisely_
+        // the low value (because of truncation)!
+        let roundtrip_nanos = then + Duration::from_nanos(NtpTime::to_nanos(expected));
+        assert!(roundtrip_nanos == low_date);
+    }
+
+    #[test]
+    fn test_simple_roundtrip_conversions() {
+        let mut test_cases: Vec<(NtpTime, DateTime<Utc>)> = vec![];
+
+        // Test case:
+        // NTP Time: 0xebbbe239 seconds and 0x3f83add9 fractions
+        // 2025-04-29 at 23:41:45.248103013 UTC and
+        let expected = NtpTime {
+            seconds: 0xebbbe239u32,
+            fractions: 0x3f83add9u32,
+        };
+        let test = Utc.with_ymd_and_hms(2025, 4, 29, 23, 41, 45).unwrap()
+            + Duration::from_nanos(248_103_013);
+
+        test_cases.push((expected, test));
+
+        // Test case:
+        // NTP Time: 0xebbd1ff2 seconds and 0x467b0671 fractions
+        // 2025-04-30 at 22:17:22.275314714 UTC and
+        let expected = NtpTime {
+            seconds: 0xebbd1ff2u32,
+            fractions: 0x467b0671u32,
+        };
+        let test = Utc.with_ymd_and_hms(2025, 4, 30, 22, 17, 22).unwrap()
+            + Duration::from_nanos(275314714);
+
+        test_cases.push((expected, test));
+
+        // Test case:
+        // NTP Time:0xebbd21b6 seconds and 0xd47290df fractions
+        // 2025-04-30 at 22:24:54.829873136 UTC and
+        let expected = NtpTime {
+            seconds: 0xebbd21b6u32,
+            fractions: 0xd47290dfu32,
+        };
+        let test = Utc.with_ymd_and_hms(2025, 4, 30, 22, 24, 54).unwrap()
+            + Duration::from_nanos(829873136);
+
+        test_cases.push((expected, test));
+
+        for (expected, test) in test_cases.iter().cloned() {
+            do_test_simple_ntp_conversion_roundtrip(expected, test);
+        }
+    }
+
+    #[test]
+    fn test_now_ntp_conversion() {
+        // This is more of a tool to help us visualize the NTP timestamp.
+        let ntp_now = NtpTime::now();
+        println!("ntp_now: {:?}", ntp_now);
+    }
+
+    #[test]
+    fn simple_ntp_conversion_from_bytes() {
+        let raw_data: [u8; 8] = [0xeb, 0xbd, 0x17, 0x88, 0x45, 0x75, 0xe9, 0x3f];
+        // Between the value in raw_data and [0xeb, 0xbd, 0x17, 0x88, 0x45, 0x75, 0xe9, 0x3f],
+        // the number of nanoseconds increases by one. That is not a usual case.
+
+        let expected: NtpTime = raw_data
+            .as_slice()
+            .try_into()
+            .expect("Should have been able to convert into nanoseconds.");
+
+        let time = Utc.with_ymd_and_hms(2025, 4, 30, 21, 41, 28).unwrap()
+            + Duration::from_nanos(271_330_430);
+
+        do_test_simple_ntp_conversion_roundtrip(expected, time);
+    }
+    #[test]
+    fn simple_ntp_raw_bytes_roundtrip_test() {
+        // Test From for NtpTime <-> Vec<u8> conversions.
+        let raw_data: [u8; 8] = [0xeb, 0xbd, 0x17, 0x88, 0x45, 0x75, 0xe9, 0x3f];
+
+        let expected: NtpTime = raw_data
+            .as_slice()
+            .try_into()
+            .expect("Should have been able to convert into nanoseconds.");
+
+        let time = Utc.with_ymd_and_hms(2025, 4, 30, 21, 41, 28).unwrap()
+            + Duration::from_nanos(271_330_430);
+
+        assert_eq!(expected.seconds, 0xebbd1788u32);
+        assert_eq!(expected.fractions, 0x4575e93fu32);
+
+        // Just for good measure.
+        do_test_simple_ntp_conversion_roundtrip(expected.clone(), time);
+
+        let re_raw_data = Into::<Vec<u8>>::into(expected);
+
+        assert!(raw_data[0..4].eq(&re_raw_data[0..4]));
+        assert!(raw_data[4..8].eq(&re_raw_data[4..8]));
+    }
 }
 
 #[derive(Clone, Debug, PartialEq)]
@@ -271,7 +387,7 @@ impl From<TimeSource> for u8 {
 
 #[cfg(test)]
 mod error_estimate_test {
-    use super::*;
+    use super::ErrorEstimate;
 
     #[test]
     pub fn test_serialization() {
